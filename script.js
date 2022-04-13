a0f74f6e679286488a850eca02ea7e9984e343f9
unknown <bensonmakau2000@gmail.com>
2022-04-13 22:53:23 +0300

    restructed open modal functionality

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+// console.log(btnsOpenModal);
+
+//Open Modal Function
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 //Looping through three buttons with the same class
 for (let i = 0; i < btnsOpenModal.length; i++) {
   //   console.log(btnsOpenModal[i].textContent);
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button Clicked');
-    modal.classList.remove('hidden');
-    //Alternative
-    // modal.style.display = 'block';
-    overlay.classList.remove('hidden');
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
 //Close Modal Function
 const closeModal = function () {
