ec91822a22a920f99680637344db60b4768bd3fe
unknown <bensonmakau2000@gmail.com>
2022-04-13 22:15:59 +0300

    Looping through three buttons with the same class

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+//Looping through three buttons with the same class
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  console.log(btnsOpenModal[i]);
+}
